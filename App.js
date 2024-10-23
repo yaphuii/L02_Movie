08a3f1f74ca6d244d847cc28f764596b494bf629
yaphuii <23004055@myrp.edu.sg>
2024-10-23 14:12:06 +0800

    NA

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StatusBar, Text, View, Image, ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
-export default function App() {
+const Movie = ({title, year, icon_name, poster}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <Image source={poster} style={{width: 400, height:500}} />
+        <Icon name={icon_name} size={18}> <Text>{title} {year}</Text></Icon>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <ScrollView>
+        <StatusBar hidden={true}/>
+      <Movie title="Doctor Sleep" year="2019" icon_name="skull" poster={require('./img/doctor-sleep.jpg')}/>
+      <Movie title="Alien Romulus" year="2024" icon_name="baby" poster={require('./img/midway.jpg')}/>
+    </ScrollView>
+  );
+}
+export default App;
